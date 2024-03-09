3ea35281a4fd0f2e9b533ffcd8a9f52cc125287b
thefankor <82701509+thefankor@users.noreply.github.com>
2024-03-09 21:00:33 +0300

    Update test.py

--- test.py
+++ test.py
@@ -8,4 +8,4 @@ TOKEN_BOT = '2137380397:AAE5M-KsMuWxMQtJftw1WA4rhYJtWLrPRz4'
 PUBLIC_ID = '@ftnotifi'
 
 bot = telebot.TeleBot(TOKEN_BOT)
-bot.send_message((PUBLIC_ID, 'TEST'))
+bot.send_message(PUBLIC_ID, 'TEST')
