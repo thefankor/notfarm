452d7720f1c3ea9426913646f578c5f806ea3520
thefankor <82701509+thefankor@users.noreply.github.com>
2024-03-15 01:03:08 +0300

    Update ska.py set 1.7

--- ska.py
+++ ska.py
@@ -79,7 +79,7 @@ while True:
     obj = get_one()
     try:
         # print(obj)
-        if obj['price']<4:
+        if obj['price']<1.7:
             print(obj)
             try:
                 MES = f"{obj['price']} - {obj['name']}\n {obj['address']}"
