42c04448cfde181fadfe3106f9eab3a731cb1e25
thefankor <82701509+thefankor@users.noreply.github.com>
2024-07-12 13:07:34 +0300

    Update ska.py ULTRA

--- ska.py
+++ ska.py
@@ -1,9 +1,41 @@
 import time
 import requests
 import json
+from urllib.parse import urlparse
 
 import telebot
 import datetime
+
+PRICE = 19.1
+
+
+def get_course():
+    url = 'https://api.cryptorank.io/v1/currencies?api_key=f4fd2781179331f3621470c4b3db233dc560de1cc2e38255204af5b432e7&ids=187369,160758'
+    r = requests.get(url)
+    val = json.loads(r.text)['data']
+    TON = 0
+    NOT = 0
+    for v in val:
+        # print(v)
+
+        if v['values']:
+            if v['values']['USD']:
+
+                if v['id'] == 187369:
+                    NOT = v['values']['USD']['price']
+                    # if type(NOT) == float:
+                        # NOT = float('{:.2f}'.format(NOT))
+                        # NOT = round(NOT, 2)
+                    # print(NOT)
+
+                elif v['id'] == 160758:
+                    TON = v['values']['USD']['price']
+                    # if type(TON) == float:
+                        # TON = round(TON, 2)
+                    #     TON = float('{:.2f}'.format(TON))
+                    # print(TON)
+
+    return {'TON': TON or 0, 'NOT': NOT or 0}
 
 
 def print_log(e):
@@ -15,7 +47,6 @@ def print_log(e):
 
 
 def get_one():
-
     url = 'https://api.getgems.io/graphql'
     headers = {
         'Content-Type': 'application/json',
@@ -47,24 +78,22 @@ def get_one():
                 ans = response.text
                 di = json.loads(ans)
                 name = di['data']['alphaNftItemSearch']['edges'][0]['node']['name']
-                price = int(di['data']['alphaNftItemSearch']['edges'][0]['node']['sale']['fullPrice'])/10**9
+                price = int(di['data']['alphaNftItemSearch']['edges'][0]['node']['sale']['fullPrice']) / 10 ** 9
                 adr_col = di['data']['alphaNftItemSearch']['edges'][0]['node']['address']
                 adr_id = di['data']['alphaNftItemSearch']['edges'][0]['node']['ownerAddress']
-                add = 'https://getgems.io/collection/'+adr_col+'/'+adr_id
+                add = 'https://getgems.io/collection/' + adr_col + '/' + adr_id
                 return {'name': name, 'price': price, 'address': add}
             except Exception as e:
                 print(e)
                 return None
         else:
             print('Ошибка запроса:', response.status_code)
-            print_log('Ошибка запроса: '+str(response.status_code))
+            print_log('Ошибка запроса: ' + str(response.status_code))
             return ('Ошибка запроса:', response.status_code)
     except Exception as ex:
         print('/RESP Ошибка запроса:', str(ex))
-        print_log('/RESP Ошибка запроса: ' +str(ex))
+        print_log('/RESP Ошибка запроса: ' + str(ex))
         return ('/RESP Ошибка запроса:', str(ex))
-
-
 
 
 TOKEN_BOT = '2137380397:AAE5M-KsMuWxMQtJftw1WA4rhYJtWLrPRz4'
@@ -74,16 +103,29 @@ try:
     ntime = '00:00'
     bot.edit_message_text(chat_id=PUBLIC_ID, message_id=3, text='starting')
 except Exception as ebot:
-    print_log('STARTBOT:::'+str(ebot))
+    print_log('STARTBOT:::' + str(ebot))
 while True:
     obj = get_one()
     try:
         # print(obj)
-        if obj['price']<16.1:
+        if obj['price'] < PRICE:
+            pr = get_course()
             print(obj)
             try:
-                MES = f"{obj['price']} - {obj['name']}\n {obj['address']}"
-                bot.send_message(PUBLIC_ID, MES)
+                PREMES = f"- 10kNOT: ${pr['NOT'] * 10000}\n- Vaucher: ${pr['TON'] * obj['price']}\n"
+                plus = pr['NOT'] * 10000 - pr['TON'] * obj['price'] - pr['TON'] * 0.3
+                if type(plus) == float:
+                    # plus = float('{:.2f}'.format(plus))
+                    plus = round(plus, 2)
+                if plus >= 15:
+                    margin = f"💚 ${plus}\n"
+                elif plus >= 10:
+                    margin = f"💛 ${plus}\n"
+                else:
+                    margin = f"❌ ${plus}\n"
+
+                MES = margin + PREMES + f"<b>{obj['price']} - {obj['name']}</b>\n {obj['address']}"
+                bot.send_message(PUBLIC_ID, MES, parse_mode='HTML')
             except:
                 bot.send_message((PUBLIC_ID, 'AAAAAAAAAAAA'))
         else:
@@ -94,7 +136,7 @@ while True:
     if n != ntime:
         ntime = n
         try:
-            bot.edit_message_text(chat_id=PUBLIC_ID, message_id=3, text=ntime+' '+str(obj['price']))
+            bot.edit_message_text(chat_id=PUBLIC_ID, message_id=3, text=ntime + ' ' + str(obj['price']))
         except:
             bot.edit_message_text(chat_id=PUBLIC_ID, message_id=3, text=ntime)
     time.sleep(22)
